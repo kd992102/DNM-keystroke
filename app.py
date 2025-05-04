980fa6f6773e232116d16409e683e8563e6726df
kd992102 <30813510+kd992102@users.noreply.github.com>
2025-05-04 12:18:08 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -94,8 +94,8 @@ def save_to_gsheet(record: dict):
         sheet_list = client.openall()
         for s in sheet_list:
             st.write("找到表單：", s.title)
+        sheet = client.open("DNM-keystroke-log").sheet1
         try:
-            sheet = client.open("DNM-keystroke-log").sheet1
             st.write("成功連結sheet")
         except Exception as e:
             st.error(f"❌ 連結 Google Sheet 失敗：{e}")
