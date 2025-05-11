e197fc25441901552751725b7ecbe20b4258f894
kd992102 <kd992102@gmail.com>
2025-05-11 17:06:32 +0800

    test

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ import streamlit as st
 from streamlit.components.v1 import html
 
 # 使用 components.html() 直接插入原生 JS 傳值
-value = html(
-    """
+html("""
     <button onclick="sendToStreamlit()">點我送資料</button>
     <script>
       function sendToStreamlit() {
@@ -15,9 +14,7 @@ value = html(
         }, "*");
       }
     </script>
-    """,
-    height=100,
-    key="simple_js"
-)
+""", height=100)
 
-st.write("JS 傳回的值是：", value)
+# Streamlit 不會自動接收回傳值，所以你應該只顯示這段 HTML
+st.info("請點上方按鈕測試 JS 傳值（目前不支援回傳值顯示）")
