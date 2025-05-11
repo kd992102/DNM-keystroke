2318f79c9e58b59685513af13b7d623b856b5147
kd992102 <kd992102@gmail.com>
2025-05-11 17:18:12 +0800

    test

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ from streamlit.components.v1 import declare_component
 import os
 
 # 設定 custom component 位置（本地開發或 Streamlit Cloud）
-_component_func = declare_component(
-    "js_input_component",
-    path=os.path.join(os.path.dirname(__file__), "my_component/frontend")
+my_component2 = declare_component(
+    name="js_input_component",
+    path="my_component/build"
 )
 
 # 取得前端傳來的值
-value = _component_func()
+value = my_component2()
 st.write("接收到的 JS 傳值：", value)
